25c3cf8dad733dc885bdbe6488623c8e07b106b3
Dennis Concepción Martín <dennisconcepcionmartin@gmail.com>
2021-08-05 11:55:55 +0100

    Added icon

---
+++ Trades/Details.swift
@@ -0,0 +1,20 @@
+//
+//  Details.swift
+//  Trades
+//
+//  Created by Dennis Concepción Martín on 5/8/21.
+//
+
+import SwiftUI
+
+struct Details: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Details_Previews: PreviewProvider {
+    static var previews: some View {
+        Details()
+    }
+}
