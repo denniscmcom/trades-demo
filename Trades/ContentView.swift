3928eb0f5f64eea133e81737f12fd3caaa552bdc
Dennis Concepción Martín <dennisconcepcionmartin@gmail.com>
2021-08-05 11:41:09 +0100

    first commit

---
+++ Trades/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trades
+//
+//  Created by Dennis Concepción Martín on 5/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Trades/TradesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradesApp.swift
+//  Trades
+//
+//  Created by Dennis Concepción Martín on 5/8/21.
+//
+
+import SwiftUI
+
+@main
+struct TradesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
